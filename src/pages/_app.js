1ba45a8b413291c548b7a13e7c3eda588cec8289
Grayson Gantek <graysonleegantek@gmail.com>
2022-10-10 19:01:03 -0400

    adds google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 import '/src/styles/global.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  )
 }
