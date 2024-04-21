a792166581809c100fbe4784c84fd1898a2eaf2c
Grayson Gantek <graysonleegantek@gmail.com>
2024-04-21 12:43:20 -0400

    removed twitter link

--- lib/data/constants.tsx
+++ lib/data/constants.tsx
@@ -1,2 +1,2 @@
-const PROFILE_LINKEDIN_URL = 'https://linkedin.com/in/graysongantek/'
-const PROFILE_GITHUB_URL = 'https://github.com/graysonlee123/'
+export const PROFILE_LINKEDIN_URL = 'https://linkedin.com/in/graysongantek/'
+export const PROFILE_GITHUB_URL = 'https://github.com/graysonlee123/'
