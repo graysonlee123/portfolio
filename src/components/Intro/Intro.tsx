f21744b8197d8d5db441ab6c686ef000c17e2ab2
Grayson Gantek <graysonleegantek@gmail.com>
2024-04-21 13:26:06 -0400

    updated images in the intro component

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,28 +3,46 @@ import classNames from 'classnames'
 import styles from './index.module.css'
 import utilStyles from '/src/styles/utils.module.scss'
 
+import laptopPic from 'public/images/laptop-01.jpg'
+import hikingPic from 'public/images/hiking.jpg'
+
 export default function Intro() {
   return (
     <section className={styles.section}>
       <div className={classNames(utilStyles.containerLg, styles.grid)}>
-        <div className={classNames(utilStyles.imgShadow, utilStyles.imgRound, styles.photo1)}>
+        <div
+          className={classNames(
+            utilStyles.imgShadow,
+            utilStyles.imgRound,
+            styles.mediaCell,
+            styles.mediaCellOne
+          )}
+        >
           <Image
-            src="/images/laptop-01.jpg"
+            className={classNames([styles.image, styles.laptop])}
+            src={laptopPic}
             alt="Photo of a laptop with code on its screen"
-            layout="fill"
-            objectFit="cover"
             sizes="(min-width: 91.125rem) 50.625rem, (min-width: 62rem) calc(((85vw - 6rem) / 3) * 2 + 3rem), 0rem"
+            placeholder="blur"
+            fill
             priority
           />
         </div>
-        <div className={classNames(utilStyles.imgShadow, utilStyles.imgRound, styles.photo2)}>
+        <div
+          className={classNames(
+            utilStyles.imgShadow,
+            utilStyles.imgRound,
+            styles.mediaCell,
+            styles.mediaCellTwo
+          )}
+        >
           <Image
-            src="/images/hiking.jpg"
-            alt="Me hiking at the Grand Canyon"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="30% 50%"
+            className={classNames([styles.image, styles.hiking])}
+            src={hikingPic}
+            alt="A photo of me hiking at the Grand Canyon"
             sizes="(min-width: 91.125rem) 23.75rem, (min-width: 62rem) calc((85vw - 6rem) / 3), (min-width: 36rem) calc((85vw - 3rem) / 2), 85vw"
+            placeholder="blur"
+            fill
             priority
           />
         </div>
