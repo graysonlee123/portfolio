b2f4ea41076f469b96cee2f6d2d3f6964b8c90f7
Grayson Gantek <graysonleegantek@gmail.com>
2024-04-23 19:10:07 -0400

    fixed contact component's image

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,25 +1,28 @@
 import ContactForm from '@/components/ContactForm'
 import Header from '@/components/Header'
 import classNames from 'classnames'
-import Image from 'next/image'
-import styles from './Contact.module.css'
-import utilStyles from '/src/styles/utils.module.scss'
 import Container from '../Container'
+import FloatingImage from '../FloatingImage/FloatingImage'
+import styles from './Contact.module.css'
+
+import outsidePic from 'public/images/outside.jpg'
 
 export default function Contact() {
   return (
     <section className={classNames(styles.section)}>
       <Container>
         <div className={styles.grid}>
-          <div className={classNames(styles.img, utilStyles.imgRound, utilStyles.imgShadow)}>
-            <Image
-              src="/images/outside.jpg"
-              alt="Me on a patio working on my laptop"
-              layout="fill"
-              objectFit="cover"
-              sizes="(min-width: 91.125rem) 46.5rem, (min-width: 48rem) calc(85vw - (85vw / 10) * (2 / 3)), 85vw"
-            />
-          </div>
+          <FloatingImage
+            className={styles.image}
+            imageProps={{
+              src: outsidePic,
+              alt: 'Me on a patio working on my laptop',
+              sizes:
+                '(min-width: 91.125rem) 46.5rem, (min-width: 48rem) calc(85vw - (85vw / 10) * (2 / 3)), 85vw',
+              fill: true,
+              placeholder: 'blur',
+            }}
+          />
           <div className={styles.form}>
             <Header header="Contact" element="h2">
               <p>
