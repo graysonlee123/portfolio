99f92fb6096ab213cf858f7a6cda283dc4b4085a
Grayson Gantek <graysonleegantek@gmail.com>
2024-04-29 17:57:12 -0400

    added content for Phasmo project

--- src/components/Columns/Columns.tsx
+++ src/components/Columns/Columns.tsx
@@ -1,7 +1,9 @@
+import classNames from 'classnames'
 import styles from './Columns.module.css'
+import textStyles from '../Text/Text.module.css'
 
 export default function Columns({ children }) {
   if (!children) return
 
-  return <div className={styles.grid}>{children}</div>
+  return <div className={classNames([styles.grid, textStyles.columns])}>{children}</div>
 }
