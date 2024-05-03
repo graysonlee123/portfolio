75d4681b0da297b000e2554323ec19ade1854e12
Grayson Gantek <graysonleegantek@gmail.com>
2024-05-03 15:34:31 -0400

    updated projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ import Skills from '@/components/Skills'
 import Intro from '@/components/Welcome'
 
 import projectPicPhasmo from 'public/images/phasmo-screenshot-01.png'
+import projectPicAxeOS from 'public/images/axeos-screenshot-01.png'
 import projectPicPrecision from 'public/images/precision-screenshot-01.png'
+import projectPicBagmart from 'public/images/bagmart-screenshot-01.png'
 
 export default function Home() {
   return (
@@ -40,13 +42,35 @@ export default function Home() {
           }
         />
         <Project
+          header="axeOS"
+          href="/projects/axeos"
+          image={projectPicAxeOS}
+          summary={
+            <p style={{ maxWidth: '56ch' }}>
+              Built for an Axe Throwing company, a full-stack application composed of a Next.js
+              front-end and a custom Node back-end.
+            </p>
+          }
+        />
+        <Project
+          header="Bagmart"
+          href="/projects/bagmart"
+          image={projectPicBagmart}
+          summary={
+            <p style={{ maxWidth: '52ch' }}>
+              A niche ecommerce project for a company named Bagmart. It features custom
+              functionality and theming.
+            </p>
+          }
+        />
+        <Project
           header="Rebuilding Precision Creative"
           href="/projects/rebuilding-precision-creative"
           image={projectPicPrecision}
           summary={
             <p style={{ maxWidth: '52ch' }}>
-              My experiences, workflow, and challenges I faced while recreating Precision Creative's
-              website for from scratch.
+              My thought process, workflow, and challenges I faced while recreating Precision
+              Creative's website for from scratch.
             </p>
           }
         />
