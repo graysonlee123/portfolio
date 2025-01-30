08929834bab129dd84fdb7ff3bc0e6c3ec0e652e
Grayson Gantek <grayson@inspry.com>
2025-01-30 17:51:46 -0500

    Enhance throttle function documentation with detailed JSDoc-style comments

--- src/lib/throttle.ts
+++ src/lib/throttle.ts
@@ -1,4 +1,12 @@
-const throttle = (cb: () => void, wait: number = 100): (() => void) => {
+/**
+ * Throttle a function to only be called once every `wait` milliseconds.
+ * 
+ * @param cb Function to throttle.
+ * @param wait Time to wait before calling the function again.
+ * 
+ * @returns A function that will call the cb function only once every `wait` milliseconds.
+ */
+const throttle = (cb: (...args: any) => void, wait: number = 100): (() => void) => {
   let time = Date.now()
 
   return function (...args) {
